fe11d288fa1f4d09c4af1c9da412c45a2030aacc
sercel23 <ercelserap@gmail.com>
2022-04-27 11:56:21 +0300

    Add splash fragment

---
+++ app/src/main/java/com/serapercel/saglikliyasam/SplashFragment.kt
@@ -0,0 +1,26 @@
+package com.serapercel.saglikliyasam
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serapercel.saglikliyasam.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding?=null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        return view
+    }
+
+}
