45dc27f29c8b462fc9a46431ff4bd7b25bedc187
sercel23 <ercelserap@gmail.com>
2022-04-27 12:07:43 +0300

    Add splash animations

--- app/src/main/java/com/serapercel/saglikliyasam/SplashFragment.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/SplashFragment.kt
@@ -1,15 +1,18 @@
 package com.serapercel.saglikliyasam
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.serapercel.saglikliyasam.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
-    private var _binding: FragmentSplashBinding?=null
+    private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -17,9 +20,11 @@ class SplashFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
         val view = binding.root
-
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 3000)
         return view
     }
 
