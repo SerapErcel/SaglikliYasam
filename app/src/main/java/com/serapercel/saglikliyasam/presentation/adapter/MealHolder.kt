485e37d1fbac86d7213e9b8f94d0e0923e6e7837
sercel23 <ercelserap@gmail.com>
2022-05-06 11:47:20 +0300

    Create meal adapter and model

---
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/adapter/MealHolder.kt
@@ -0,0 +1,7 @@
+package com.serapercel.saglikliyasam.presentation.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.serapercel.saglikliyasam.databinding.MealRecyclerRowBinding
+
+class MealHolder(val binding: MealRecyclerRowBinding) : RecyclerView.ViewHolder(binding.root) {
+}
