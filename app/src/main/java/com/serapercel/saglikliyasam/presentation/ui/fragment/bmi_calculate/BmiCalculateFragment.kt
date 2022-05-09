accb74d7b73d6168e031b2f8ffee64612677eb2a
sercel23 <ercelserap@gmail.com>
2022-05-09 13:51:20 +0300

    Fix bmi calculate results

--- app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/bmi_calculate/BmiCalculateFragment.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/bmi_calculate/BmiCalculateFragment.kt
@@ -1,11 +1,13 @@
 package com.serapercel.saglikliyasam.presentation.ui.fragment.bmi_calculate
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import com.serapercel.saglikliyasam.databinding.FragmentBmiCalculateBinding
+
 
 class BmiCalculateFragment : Fragment() {
     private var _binding: FragmentBmiCalculateBinding? = null
@@ -16,6 +18,10 @@ class BmiCalculateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBmiCalculateBinding.inflate(inflater, container, false)
+        binding.bmiLayout.isVisible = false
+        binding.calculateButton.setOnClickListener {
+            binding.bmiLayout.isVisible = true
+        }
         return binding.root
     }
 
