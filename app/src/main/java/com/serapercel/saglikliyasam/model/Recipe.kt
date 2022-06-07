fe9047b0732a3d41e5347c001919ed8d085fc058
sercel23 <ercelserap@gmail.com>
2022-06-07 19:08:48 +0300

    Create recipe data class

--- app/src/main/java/com/serapercel/saglikliyasam/model/Recipe.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/model/Recipe.kt
@@ -1,14 +1,36 @@
 package com.serapercel.saglikliyasam.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
 var recipeList = mutableListOf<Recipe>()
 
-val RECIPE_ID_EXTRA = "recipeExtra"
-
-class Recipe(
+@Entity
+data class Recipe(
     var cover: Int,
+    @ColumnInfo(name = "isim")
+    @SerializedName("isim")
     var name: String,
+    @ColumnInfo(name = "sure")
+    @SerializedName("sure")
     var time: String,
+    @ColumnInfo(name = "malzemeler")
+    @SerializedName("malzemeler")
     var necessaries: String,
+    @ColumnInfo(name = "tarif")
+    @SerializedName("tarif")
     var description: String,
-    val id: Int? = recipeList.size
-)
+
+
+    @ColumnInfo(name = "gorsel")
+    @SerializedName("gorsel")
+    val yemekGorsel: String?
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+
+}
+
