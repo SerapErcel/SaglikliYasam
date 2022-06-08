04dd1f4faf0a48af1b570786f1592f475344e803
sercel23 <ercelserap@gmail.com>
2022-06-08 23:35:34 +0300

    Create recipe DAO and room database

---
+++ app/src/main/java/com/serapercel/saglikliyasam/database/RecipeDAO.kt
@@ -0,0 +1,22 @@
+package com.serapercel.saglikliyasam.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.serapercel.saglikliyasam.model.Recipe
+
+@Dao
+interface RecipeDAO {
+    //Data Access Object
+    @Insert
+    suspend fun insertAll(vararg recipe: Recipe): List<Long>
+
+    @Query("SELECT * FROM Recipe")
+    suspend fun getAllRecipe(): List<Recipe>
+
+    @Query("SELECT * FROM Recipe WHERE uuid = :recipeId")
+    suspend fun getRecipe(recipeId: Int): Recipe
+
+    @Query("DELETE FROM Recipe")
+    suspend fun deleteAllRecipe()
+}
