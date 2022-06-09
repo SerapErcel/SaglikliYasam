0a833c002f1c95002542032a2805fd7152b526c1
sercel23 <ercelserap@gmail.com>
2022-06-10 01:45:06 +0300

    Add exercise get data from sqlite function

--- app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/exercises/ExercisesViewModel.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/exercises/ExercisesViewModel.kt
@@ -1,24 +1,50 @@
 package com.serapercel.saglikliyasam.presentation.ui.fragment.exercises
 
-import androidx.lifecycle.ViewModel
-import com.serapercel.saglikliyasam.R
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.serapercel.saglikliyasam.database.RDatabase
+import com.serapercel.saglikliyasam.database.SharedPreferences
 import com.serapercel.saglikliyasam.model.Exercise
-import com.serapercel.saglikliyasam.model.exerciseList
+import com.serapercel.saglikliyasam.service.ExerciseAPIService
+import com.serapercel.saglikliyasam.util.base.BaseViewModel
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.launch
 
-class ExercisesViewModel : ViewModel() {
-    // todo "change mock data with real data"
-    fun populateExercises() {
-        val exercise1 = Exercise(
-            R.drawable.patlican_pizza,
-            name = "Isınma Egzersizi",
-            time = "3 dakika",
-            description = "Isınma egzersizini verilen süre boyunca bulunduğunuz yerde tekrar tekar uygulayınız. "
-        )
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
+class ExercisesViewModel(application: Application) : BaseViewModel(application) {
+
+    val exercises = MutableLiveData<List<Exercise>>()
+    val exerciseErrorMessage = MutableLiveData<Boolean>()
+    val exerciseDownloading = MutableLiveData<Boolean>()
+
+    private var updateTime = 10 * 60 * 1000 * 1000 * 1000L
+    private val exerciseAPIService = ExerciseAPIService()
+    private val disposable = CompositeDisposable()
+    private val SharedPreferences = SharedPreferences(getApplication())
+
+    fun refreshData() {
+        val saveTime = SharedPreferences.getTime()
+        if (saveTime != null && saveTime != 0L && System.nanoTime() - saveTime < updateTime) {
+            getDataFromSQLite()
+        } else {
+            getDataFromInternet()
+        }
+    }
+
+    private fun getDataFromInternet() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getDataFromSQLite() {
+        exerciseDownloading.value = true
+        launch {
+            val exerciseList = RDatabase(getApplication()).exerciseDao().getAllExercise()
+            showExercises(exerciseList)
+        }
+    }
+
+    private fun showExercises(exerciseList: List<Exercise>) {
+        exercises.value = exerciseList
+        exerciseErrorMessage.value = false
+        exerciseDownloading.value = false
     }
 }
