60b78acff5fe476eb2f8a0a75b87e203dd70ebd2
sercel23 <ercelserap@gmail.com>
2022-06-10 01:21:00 +0300

    Add exercise database create function

--- app/src/main/java/com/serapercel/saglikliyasam/database/RDatabase.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/database/RDatabase.kt
@@ -4,9 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.serapercel.saglikliyasam.model.Exercise
 import com.serapercel.saglikliyasam.model.Recipe
 
-@Database(entities = [Recipe::class], version = 1, exportSchema = false)
+@Database(entities = [Recipe::class, Exercise::class], version = 1, exportSchema = false)
 abstract class RDatabase : RoomDatabase() {
     abstract fun recipeDao(): RecipeDAO
 
@@ -29,5 +30,10 @@ abstract class RDatabase : RoomDatabase() {
             "Recipe"
         ).build()
 
+        private fun createDatabaseExercise(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            RDatabase::class.java,
+            "Exercise"
+        ).build()
     }
 }
