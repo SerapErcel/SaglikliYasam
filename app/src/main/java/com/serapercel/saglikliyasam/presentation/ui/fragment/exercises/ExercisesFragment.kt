b7d09443269b39055d004bd3f00bfaf41dde3939
sercel23 <ercelserap@gmail.com>
2022-06-10 21:58:09 +0300

    Update exercise on view created

--- app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/exercises/ExercisesFragment.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/exercises/ExercisesFragment.kt
@@ -5,16 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import com.serapercel.saglikliyasam.R
 import com.serapercel.saglikliyasam.databinding.FragmentExercisesBinding
 import com.serapercel.saglikliyasam.model.Exercise
-import com.serapercel.saglikliyasam.model.exerciseList
 import com.serapercel.saglikliyasam.presentation.adapter.ExerciseCardAdapter
 import com.serapercel.saglikliyasam.util.listener.ExerciseClickListener
 
 class ExercisesFragment : Fragment(), ExerciseClickListener {
+
+    private lateinit var viewModel: ExercisesViewModel
 
     private var _binding: FragmentExercisesBinding? = null
     private val binding get() = _binding!!
@@ -27,21 +28,26 @@ class ExercisesFragment : Fragment(), ExerciseClickListener {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        populateExercises()
+        viewModel = ViewModelProvider(requireActivity()).get(ExercisesViewModel::class.java)
+        viewModel.refreshData()
 
         binding.exerciseRecyclerView.apply {
-            layoutManager = GridLayoutManager(context, 1)
-            adapter = ExerciseCardAdapter(exerciseList, this@ExercisesFragment)
+            layoutManager = GridLayoutManager(requireContext(), 1)
+            adapter = ExerciseCardAdapter(emptyList(), this@ExercisesFragment, requireContext())
         }
+        observeLiveData()
+    }
+
+    private fun observeLiveData() {
+        TODO("Not yet implemented")
     }
 
     override fun onClick(exercise: Exercise) {
         val action = ExercisesFragmentDirections.actionExercisesFragmentToExerciseDetailFragment(
-            exercise.id!!
+            exercise.uuid!!
         )
         findNavController().navigate(action)
     }
@@ -51,21 +57,6 @@ class ExercisesFragment : Fragment(), ExerciseClickListener {
         _binding = null
     }
 
-    // todo "change mock data with real data"
-    private fun populateExercises() {
-        val exercise1 = Exercise(
-            R.drawable.exercise,
-            name = "Isınma Egzersizi",
-            time = "3 dakika",
-            description = "Isınma egzersizini verilen süre boyunca bulunduğunuz yerde tekrar tekar uygulayınız. "
-        )
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-        exerciseList.add(exercise1)
-    }
 }
 
 
