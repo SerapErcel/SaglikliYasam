6a431965b5a8f1cbac77ed809d88cd12372ed413
sercel23 <ercelserap@gmail.com>
2022-06-10 21:32:59 +0300

    Add exercise get data from Internet function

--- app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/exercises/ExercisesViewModel.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/exercises/ExercisesViewModel.kt
@@ -1,13 +1,18 @@
 package com.serapercel.saglikliyasam.presentation.ui.fragment.exercises
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.serapercel.saglikliyasam.database.RDatabase
 import com.serapercel.saglikliyasam.database.SharedPreferences
 import com.serapercel.saglikliyasam.model.Exercise
+import com.serapercel.saglikliyasam.model.Recipe
 import com.serapercel.saglikliyasam.service.ExerciseAPIService
 import com.serapercel.saglikliyasam.util.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.launch
 
 class ExercisesViewModel(application: Application) : BaseViewModel(application) {
@@ -31,6 +36,29 @@ class ExercisesViewModel(application: Application) : BaseViewModel(application) 
     }
 
     private fun getDataFromInternet() {
+        exerciseDownloading.value = true
+        disposable.add(
+            exerciseAPIService.getExerciseData()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<Exercise>>() {
+                    override fun onSuccess(t: List<Exercise>) {
+                        storeInSQLite(t)
+                        Toast.makeText(getApplication(), "Liste Güncellendi", Toast.LENGTH_SHORT)
+                            .show()
+                        exercises.value = t
+                    }
+
+                    override fun onError(e: Throwable) {
+                        exerciseErrorMessage.value = true
+                        exerciseDownloading.value = false
+                        e.printStackTrace()
+                    }
+                })
+        )
+    }
+
+    private fun storeInSQLite(t: List<Exercise>) {
         TODO("Not yet implemented")
     }
 
