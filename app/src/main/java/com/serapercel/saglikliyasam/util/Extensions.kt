b9bf997cec17c9a92c867764c2dcbf701ebe9c32
sercel23 <ercelserap@gmail.com>
2022-06-12 14:28:59 +0300

    Add create work request extension

--- app/src/main/java/com/serapercel/saglikliyasam/util/Extensions.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/util/Extensions.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.serapercel.saglikliyasam.R
+import java.util.concurrent.TimeUnit
 
 fun Context.toastShort(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -27,4 +31,18 @@ fun placeHolder(context: Context): CircularProgressDrawable {
         centerRadius = 40f
         start()
     }
+}
+
+fun createWorkRequest(message: String, timeDelay: Long, context: Context) {
+    val myWorkRequest = PeriodicWorkRequestBuilder<ReminderWorker>(timeDelay, TimeUnit.MINUTES)
+        .setInputData(
+            workDataOf(
+                "title" to "Sağlıklı Yaşam",
+                "message" to message,
+            )
+        )
+        .build()
+
+    WorkManager.getInstance(context).enqueue(myWorkRequest)
+
 }
