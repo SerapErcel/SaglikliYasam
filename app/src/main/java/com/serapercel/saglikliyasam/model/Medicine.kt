77ebdbdcdcab0add1d302979653383ff835ce127
sercel23 <ercelserap@gmail.com>
2022-06-13 23:57:57 +0300

    Update medicine model

--- app/src/main/java/com/serapercel/saglikliyasam/model/Medicine.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/model/Medicine.kt
@@ -1,9 +1,21 @@
 package com.serapercel.saglikliyasam.model
 
-var medicineList = mutableListOf<Medicine>()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
-class Medicine(
+@Entity(tableName = "Medicines")
+data class Medicine(
+    @ColumnInfo(name = "medicineName")
+    @SerializedName("isim")
     var name: String,
+    @ColumnInfo(name = "medicineTime")
+    @SerializedName("sure")
     var time: String,
-    val id: Int? = medicineList.size
-)
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+
+}
