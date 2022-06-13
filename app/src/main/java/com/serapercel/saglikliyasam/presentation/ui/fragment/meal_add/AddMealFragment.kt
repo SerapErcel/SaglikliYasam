3b7865493c67229854ff74828be93b3cc42796a2
sercel23 <ercelserap@gmail.com>
2022-06-14 01:36:01 +0300

    Create add meal to database

--- app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/meal_add/AddMealFragment.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/meal_add/AddMealFragment.kt
@@ -6,10 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.serapercel.saglikliyasam.databinding.FragmentAddMealBinding
+import com.serapercel.saglikliyasam.model.Meal
 import com.serapercel.saglikliyasam.util.createDailyWorkRequest
 
 class AddMealFragment : Fragment() {
+
+    private lateinit var viewModel: MealAddViewModel
+
 
     private var _binding: FragmentAddMealBinding? = null
     private val binding get() = _binding!!
@@ -31,6 +37,7 @@ class AddMealFragment : Fragment() {
         val button = binding.addButton
         val timePicker = binding.timePicker
 
+        viewModel = ViewModelProvider(requireActivity()).get(MealAddViewModel::class.java)
 
         timePicker.setOnTimeChangedListener { _, hour, minute ->
             chosenHour = hour
@@ -44,8 +51,19 @@ class AddMealFragment : Fragment() {
                 descriptionText.text.toString(), requireContext()
             )
 
+            viewModel.storeInSQLite(
+                Meal(
+                    descriptionText.text.toString(),
+                    "$chosenHour:$chosenMin"
+                )
+            )
+
             Toast.makeText(requireContext(), "Öğün Hatırlatıcı Oluşturuldu!", Toast.LENGTH_SHORT)
                 .show()
+
+            val action =
+                AddMealFragmentDirections.actionAddMealFragmentToMealsFragment()
+            findNavController().navigate(action)
         }
     }
 
