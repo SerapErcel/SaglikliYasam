3a64c477a7cca207c458ba4e128248b40b7a88ff
sercel23 <ercelserap@gmail.com>
2022-06-14 01:51:24 +0300

    Fix database folder

--- app/src/main/java/com/serapercel/saglikliyasam/database/RDatabase.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/database/RDatabase.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.serapercel.saglikliyasam.database.dao.ExerciseDAO
+import com.serapercel.saglikliyasam.database.dao.MealDAO
+import com.serapercel.saglikliyasam.database.dao.MedicineDAO
+import com.serapercel.saglikliyasam.database.dao.RecipeDAO
 import com.serapercel.saglikliyasam.model.Exercise
 import com.serapercel.saglikliyasam.model.Meal
 import com.serapercel.saglikliyasam.model.Medicine
