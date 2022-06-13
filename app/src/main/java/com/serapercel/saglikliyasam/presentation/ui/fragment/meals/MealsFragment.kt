41a7ff1e5163ff94d746c85695ae5189cc0cae02
sercel23 <ercelserap@gmail.com>
2022-06-14 01:36:15 +0300

    Add meal listing

--- app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/meals/MealsFragment.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/meals/MealsFragment.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.serapercel.saglikliyasam.R
 import com.serapercel.saglikliyasam.databinding.FragmentMealsBinding
 import com.serapercel.saglikliyasam.model.Meal
-import com.serapercel.saglikliyasam.model.mealList
 import com.serapercel.saglikliyasam.presentation.adapter.MealAdapter
+import com.serapercel.saglikliyasam.presentation.adapter.MedicineAdapter
 
 class MealsFragment : Fragment() {
+
+    private lateinit var viewModel: MealsViewModel
 
     private var _binding: FragmentMealsBinding? = null
     private val binding get() = _binding!!
@@ -28,13 +31,34 @@ class MealsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        populateMeal()
-        binding.mealsRecyclerView.layoutManager = LinearLayoutManager(context)
-        val mealAdapter = MealAdapter()
-        binding.mealsRecyclerView.adapter = mealAdapter
+
+        viewModel = ViewModelProvider(requireActivity()).get(MealsViewModel::class.java)
+        viewModel.getDataFromSQLite()
+
+        binding.mealsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = MedicineAdapter(emptyList())
+        }
+
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_mealsFragment_to_addMealFragment)
         }
+
+        observeLiveData()
+
+    }
+
+    private fun observeLiveData() {
+        viewModel.meals.observe(viewLifecycleOwner) { mealList ->
+            mealList?.let {
+                updateRecyclerList(mealList)
+            }
+        }
+    }
+
+    private fun updateRecyclerList(mealList: List<Meal>) {
+        binding.mealsRecyclerView.adapter =
+            MealAdapter(mealList)
 
     }
 
@@ -42,20 +66,4 @@ class MealsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-    // todo "change mock data with real data"
-    private fun populateMeal() {
-        val meal1 = Meal(
-            name = "Kahvaltı",
-            time = "08:00"
-        )
-        mealList.add(meal1)
-        mealList.add(meal1)
-        mealList.add(meal1)
-        mealList.add(meal1)
-        mealList.add(meal1)
-        mealList.add(meal1)
-
-    }
-
 }
--- app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/meals/MealsViewModel.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/ui/fragment/meals/MealsViewModel.kt
@@ -1,6 +1,25 @@
 package com.serapercel.saglikliyasam.presentation.ui.fragment.meals
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.serapercel.saglikliyasam.database.RDatabase
+import com.serapercel.saglikliyasam.model.Meal
+import com.serapercel.saglikliyasam.util.base.BaseViewModel
+import kotlinx.coroutines.launch
 
-class MealsViewModel : ViewModel(){
+class MealsViewModel(application: Application) : BaseViewModel(application) {
+
+    val meals = MutableLiveData<List<Meal>>()
+
+    fun getDataFromSQLite() {
+        launch {
+            val mealList = RDatabase(getApplication()).mealDao().getAllMeal()
+            showMeals(mealList)
+        }
+    }
+
+    private fun showMeals(mealList: List<Meal>) {
+        meals.value = mealList
+
+    }
 }
