5b28eed901e479cfa590070022f23e78ead1d7a7
sercel23 <ercelserap@gmail.com>
2022-06-14 01:17:07 +0300

    Update meal adapter

--- app/src/main/java/com/serapercel/saglikliyasam/presentation/adapter/MealAdapter.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/adapter/MealAdapter.kt
@@ -4,9 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.serapercel.saglikliyasam.databinding.MealRecyclerRowBinding
-import com.serapercel.saglikliyasam.model.mealList
+import com.serapercel.saglikliyasam.model.Meal
 
-class MealAdapter : RecyclerView.Adapter<MealHolder>() {
+class MealAdapter(
+    private val meals: List<Meal>,
+) : RecyclerView.Adapter<MealHolder>(
+
+) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MealHolder {
         val binding =
             MealRecyclerRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -14,11 +18,9 @@ class MealAdapter : RecyclerView.Adapter<MealHolder>() {
     }
 
     override fun onBindViewHolder(holder: MealHolder, position: Int) {
-        holder.binding.mealText.text = mealList[position].name
-        holder.binding.mealTimeText.text = mealList[position].time
+        holder.bindMeal(meals[position])
     }
 
-    override fun getItemCount(): Int {
-        return mealList.size
-    }
+    override fun getItemCount(): Int = meals.size
+
 }
--- app/src/main/java/com/serapercel/saglikliyasam/presentation/adapter/MealHolder.kt
+++ app/src/main/java/com/serapercel/saglikliyasam/presentation/adapter/MealHolder.kt
@@ -2,6 +2,12 @@ package com.serapercel.saglikliyasam.presentation.adapter
 
 import androidx.recyclerview.widget.RecyclerView
 import com.serapercel.saglikliyasam.databinding.MealRecyclerRowBinding
+import com.serapercel.saglikliyasam.model.Meal
 
 class MealHolder(val binding: MealRecyclerRowBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bindMeal(meal: Meal) {
+        binding.mealText.text = meal.name
+        binding.mealTimeText.text = meal.time
+    }
 }
